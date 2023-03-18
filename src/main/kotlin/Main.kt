5592b4af83635d511b0a8cc9e27a7b24b435d876
Daniela MG <lidamagu19@gmail.com>
2023-03-18 15:51:10 -0400

    Tipos de datos

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,12 @@
 fun main() {
    println("Hola bienvenidos al Modulo del Diplomado")
-}
+   var num:Int=20
+   num=4
+   println("El número es: $num")
+   var num2:Double=3.1416
+   println("El valor de pi es: $num2")
+   var texto:String="Hola mundo"
+   println(texto)
+   var verdadero:Boolean=true
+   println("La variable verdadero es: $verdadero")
+   }
