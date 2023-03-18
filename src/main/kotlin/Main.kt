fe17caae3862807899bc263832f702d0302b58e7
Daniela MG <lidamagu19@gmail.com>
2023-03-18 16:07:48 -0400

    Condicionales

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -9,4 +9,19 @@ fun main() {
    println(texto)
    var verdadero:Boolean=true
    println("La variable verdadero es: $verdadero")
+   if(num%2==0){
+      println("El número es par")
+   }else{
+      println("El número es impar")
    }
+   when(num){
+      1-> println("Lunes")
+      2-> println("Martes")
+      3-> println("Miércoles")
+      4-> println("Jueves")
+      5-> println("Viernes")
+      6-> println("Sábado")
+      7-> println("Domingo")
+      else-> println("Error, número inválido")
+   }
+}
