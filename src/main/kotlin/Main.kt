8256826249cb0dfd0291f5bdbd0c33d942b2a530
Jack625a <ktfut15@gmail.com>
2023-03-17 21:14:57 -0400

    Variable y tipos de datos en Kotlin

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,11 @@
 fun main() {
-   println("Hola bienvenidos al Modulo del Diplomado")
+   var num: Int=25
+   num=100
+   println("El numero es: $num")
+   var num2: Double=3.1416
+   println("El valor de pi es: $num2")
+   var texto: String="Hola mundo"
+   println(texto)
+   var verdadero:Boolean=true
+   println("La variable es verdadero: $verdadero")
 }
