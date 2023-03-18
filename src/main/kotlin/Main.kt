1c61062f9406de8f485f0ead13a8d09b38f426e4
jodasis <jdavidqp@gmail.com>
2023-03-17 21:22:46 -0400

    Update Main.kt
    variables y tipos de datos en kotlin

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,12 @@
 fun main() {
    println("Hola bienvenidos al Modulo del Diplomado")
+
+   var num: Int=25
+   println("El numero es: $num")
+   var num2: Double = 3.1416
+   println("El valor de pi es: $num2")
+   var texto: String = "david"
+   println("Hola $texto")
+   var valor: Boolean = true
+   println("la variable es: $valor")
 }
