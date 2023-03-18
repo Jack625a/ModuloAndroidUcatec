fa3cb31b729811e34c0f44e6d1136c3d1f9bb035
jodasis <jdavidqp@gmail.com>
2023-03-17 22:51:41 -0400

    Kotlin While Loop

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -15,4 +15,44 @@ fun main() {
    } else {
       println("El número $num es IMPAR")
    }
+
+   var dia = 5
+   when (dia) {
+       1 -> println("Lunes")
+       2 -> println("Martes")
+       3 -> println("Miércoles")
+       4 -> println("Jueves")
+       5 -> println("Viernes")
+       6 -> println("Sábado")
+       7 -> println("Domingo")
+       else -> println("dia invalido.")
+   }
+
+
+   var cont = 1
+   while (cont <= 10) {
+       println(cont)
+       cont++
+   }
+
+   var suma=0
+   var numero: Int
+   print("Ingrese un numero o ingrese 0 para salir: ")
+   while (true) {
+       numero = readLine()?.toInt() as Int
+       if (numero == 0) {
+           break
+       }
+       suma = suma + numero
+       println("Ingrese un numero o 0 para salir: ")
+   }
+   println("La suma de los numeros ingresados es: $suma")
+
+   println("Tabla de multiplicar del 8")
+   var acumulador = 1
+   while (acumulador<=10) {
+       println("8x $acumulador = ${8*acumulador}")
+       acumulador++
+   }
+
 }
